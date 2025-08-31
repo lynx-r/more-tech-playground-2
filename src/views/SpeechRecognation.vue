972692941f2c6a717749dab57e73ab3778ee9c7c
Aleksey P <aleksey.p10n@gmail.com>
2025-08-31 17:45:16 +0300

    speech

---
+++ src/views/SpeechRecognation.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import useMySpeechRecognition from './useMySpeechRecognition'
+
+const { isSupported, isListening, result, start, stop } = useMySpeechRecognition()
+</script>
+
+<template>
+  <div>
+    <div v-if="!isSupported">
+      Your browser does not support SpeechRecognition API,
+      <a href="https://caniuse.com/mdn-api_speechrecognition" target="_blank">more details</a>
+    </div>
+    <div v-else>
+      <button v-if="!isListening" @click="start">Press and talk</button>
+      <button v-if="isListening" class="orange" @click="stop">Stop</button>
+      <div v-if="isListening" class="mt-4">
+        <p class="tag">
+          {{ result }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/SpeechSynthesis.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import useMySpeechSynthesis from './useMySpeechSynthesis'
+
+const { speech, textSegments, text, pause, play, stop } = useMySpeechSynthesis()
+</script>
+
+<template>
+  <div>
+    <div v-if="!speech.isSupported">
+      Your browser does not support SpeechSynthesis API,
+      <a href="https://caniuse.com/mdn-api_speechsynthesis" target="_blank">more details</a>
+    </div>
+    <div v-else>
+      <label class="font-bold mr-2">Spoken Text</label>
+      <input v-model="text" class="!inline-block" type="text" />
+      <div class="mt-2" aria-label="current-boundary-preview">
+        <label class="font-bold mr-2">Speaking Text</label>
+        <span>{{ textSegments.leadingText }}</span>
+        <span class="text-primary">{{ textSegments.highlightedText }}</span>
+        <span>{{ textSegments.trailingText }}</span>
+      </div>
+
+      <div class="mt-2">
+        <button :disabled="speech.isPlaying.value" @click="play">
+          {{ speech.status.value === 'pause' ? 'Resume' : 'Speak' }}
+        </button>
+        <button :disabled="!speech.isPlaying.value" class="orange" @click="pause">Pause</button>
+        <button :disabled="!speech.isPlaying.value" class="red" @click="stop">Stop</button>
+      </div>
+    </div>
+  </div>
+</template>
